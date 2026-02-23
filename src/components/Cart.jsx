b4f092caf99d54378f7191cdc22161fc72fc73bb
ayorufai93 <ayorufai93@gmail.com>
2026-02-23 13:28:04 +0100

    adjusted Cart.jsx file

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import CartItem from "./CartItem";
-import Modal from "./Modal";
+import Modal from "./modal.jsx";
 
 function Cart({ cart, setCart }) {
   const [showModal, setShowModal] = useState(false);
